a0c1ce5d9c91c58709450b2cb6422e4398526c48
kentkentj <48156569+kentkentj@users.noreply.github.com>
2022-06-16 12:40:26 +0800

    updated js cdn

--- template/footer.php
+++ template/footer.php
@@ -1,5 +1,10 @@
  <!-- MDB -->
  <script type="text/javascript" src="js/mdb.min.js"></script>
+ <!-- MDB -->
+<script
+  type="text/javascript"
+  src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.2.0/mdb.min.js"
+></script>
 </body>
 
 <script>
