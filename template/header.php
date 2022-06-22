46c506b7037240f4f28f06d58cde94f099917c09
kentkentj <48156569+kentkentj@users.noreply.github.com>
2022-06-22 11:02:35 +0800

    dashboard sidebar configure

--- template/header.php
+++ template/header.php
@@ -16,7 +16,7 @@
     />
     <!-- MDB -->
     <link rel="stylesheet" href="http://<?php echo $_SERVER['HTTP_HOST']  ?>/cit8UcSpecial-master/css/mdb.min.css" />
-
+    <link rel="stylesheet" href="http://<?php echo $_SERVER['HTTP_HOST']  ?>/cit8UcSpecial-master/css/admin.css" />
     <link rel="stylesheet" href="http://<?php echo $_SERVER['HTTP_HOST']  ?>/cit8UcSpecial-master/css/style.css" />
 
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
