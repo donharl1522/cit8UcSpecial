4d570da3e80b4b71477ebfc955bcb0a30036cbce
kentkentj <48156569+kentkentj@users.noreply.github.com>
2022-07-04 15:18:17 +0800

    agricultural history data change button color

--- historicaldata/agricultural/index.php
+++ historicaldata/agricultural/index.php
@@ -907,6 +907,7 @@
                     Swal.fire({
                     icon: 'success',
                     title: 'Says',
+                    confirmButtonColor: '#0e2f36',
                     text: '<?php echo $prodRateRow["munName"]." is the Top Producer of ". $cropSelectedTop. " During " .$yearSearchedFromTop." to ".$yearSearchedToTop; ?>'
                     })
                 </script>
