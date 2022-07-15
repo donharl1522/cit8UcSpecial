440b9af75eeabbd74ae25313a758ca24e34e9daf
kentkentj <48156569+kentkentj@users.noreply.github.com>
2022-07-15 22:33:13 +0800

    Add files via upload

--- index2.php
+++ index2.php
@@ -11,7 +11,7 @@
         <div class="card" style="border-radius: 1rem;">
           <div class="row g-0">
             <div class="col-md-6 col-lg-5 d-none d-md-block">
-              <img src="https://thecounter.org/wp-content/uploads/2021/05/regenerative-agriculture-access-climate-change-justice.jpg"
+              <img src="images/Benguet-farm-1.png"
                 alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
             </div>
             <div class="col-md-6 col-lg-7 d-flex align-items-center">
