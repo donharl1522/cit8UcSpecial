3b94241db78368a1a5a1d4333b1ca7bce029e08c
donharl1522 <74662519+donharl1522@users.noreply.github.com>
2022-07-16 03:38:19 +0800

    Delete top.php

--- top.php
+++
@@ -1,6 +0,0 @@
-$query= "SELECT MAX( prodRate ) AS 'topCrop' FROM `historicaldataset` WHERE cropName = '$cropSelected' and harvestYear between '$yearSearchedFrom' and '$yearSearchedTo'";
-                        $res1=mysqli_query($link, $query);
-                        $topCropData = mysqli_fetch_array( $res1);
-                        echo "The".$topCropData['topCrop'];
-
-                        
