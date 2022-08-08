f73a2961bf74e4fbebbce7597769aa3a448cb918
kentkentj <48156569+kentkentj@users.noreply.github.com>
2022-08-08 11:17:00 +0800

    predictions - news - community

---
+++ dashboard/predictions/index.php
@@ -0,0 +1,24 @@
+<?php
+   // We need to use sessions, so you should always start sessions using the below code.
+   session_start();
+   // If the user is not logged in redirect to the login page...
+   if (!isset($_SESSION['loggedin'])) {
+   	header('Location: ../../login.php');
+   	exit;
+   	//blah blah blah
+   }
+   ?>
+<?php require "../../config/connection.php"; ?>
+<?php include '../../template/header.php'; ?>
+<?php include '../../template/sidebar.php'; ?>
+<!--Main layout-->
+<main style="margin-top: 58px;">
+   <div class="container pt-4">
+      <section class="vw-25">
+        <h1>Crop Prediction</h1>
+      </section>   
+   </div>
+</main>
+<!--Main layout-->
+
+<?php include '../../template/footer.php'; ?>
