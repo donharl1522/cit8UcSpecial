1805124eaa079bf9403385599764254b142db52a
kentkentj <48156569+kentkentj@users.noreply.github.com>
2022-08-09 22:22:06 +0800

    fix

--- template/footer.php
+++ template/footer.php
@@ -1,23 +1,20 @@
- <!-- MDB -->
- <script type="text/javascript" src="js/mdb.min.js"></script>
- <!-- MDB -->
-<script
-  type="text/javascript"
-  src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.2.0/mdb.min.js"
-></script>
+    <!-- MDB -->
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+    <!-- MDB -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.2.0/mdb.min.js"></script>
+    <!-- Vendor JS Files -->
+    <script src="css/welcome-page/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="css/welcome-page/vendor/aos/aos.js"></script>
+    <script src="css/welcome-page/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="css/welcome-page/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="css/welcome-page/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="css/welcome-page/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="css/welcome-page/vendor/php-email-form/validate.js"></script>
 
-<!--DataTable-->
-<script>
-    $(document).ready(function() {
-    var table = $('#example').DataTable( {
-        lengthChange: false,
-        buttons: [ 'copy', 'excel', 'pdf', 'colvis' ]
-    } );
- 
-    table.buttons().container()
-        .appendTo( '#example_wrapper .col-md-6:eq(0)' );
-} );
-</script>
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+
 </body>
 
 <script>
---
+++ template/guest-page-header.php
@@ -0,0 +1,37 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>PASYA - Home</title>
+  <meta content="" name="description">
+
+  <meta content="" name="keywords">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="vendor/aos/aos.css" rel="stylesheet">
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="http://<?php echo $_SERVER['HTTP_HOST']  ?>/cit8UcSpecial-master/css/style.css" />
+
+  <!-- Template Main CSS File -->
+  <link href="css/homepage.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: FlexStart - v1.10.1
+  * Template URL: https://bootstrapmade.com/flexstart-bootstrap-startup-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
