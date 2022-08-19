4549b49d2832e59548488ac7f4d918df1c80131a
kentkentj <48156569+kentkentj@users.noreply.github.com>
2022-08-19 10:36:31 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
       <div class="row">
         <div class="col-lg-6 d-flex flex-column justify-content-center">
           <!--<h1 data-aos="fade-up">PASYA</h1>-->
-          <h2 data-aos="fade-up" data-aos-delay="400">Harvest the Power of Smart Farming</h2>
+          <!--<h2 data-aos="fade-up" data-aos-delay="400">Harvest the Power of Smart Farming</h2>-->
           <div data-aos="fade-up" data-aos-delay="600">
             <div class="text-center text-lg-start">
               <a href="#about" class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
@@ -20,6 +20,7 @@
         </div>
         <div class="col-lg-6 hero-img" data-aos="zoom-out" data-aos-delay="200">
           <img src="images/logo/logob2.png" class="img-fluid" alt="">
+          <h2 data-aos="fade-up" data-aos-delay="400">Harvest the Power of Smart Farming</h2>
         </div>
       </div>
     </div>
